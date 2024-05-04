58be6511682905e98911569db1787bbbf53fedb8
DandieKYT <75677738+DandieKYT@users.noreply.github.com>
2024-05-04 13:14:52 +0300

    new mobile

--- src/main/java/mobile/config/BrowserstackConfig.java
+++ src/main/java/mobile/config/BrowserstackConfig.java
@@ -3,7 +3,7 @@ package mobile.config;
 import org.aeonbits.owner.Config;
 
 @Config.Sources({
-        "file:src/test/resources/config/test.properties"
+        "file:src/main/resources/config/test.properties"
 })
 public interface BrowserstackConfig extends Config {
 
